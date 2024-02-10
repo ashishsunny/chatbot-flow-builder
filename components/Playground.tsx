81b1abb49fbd7a01a3d49d003fb855b1640810fe
ashishsunny <ashishsunny.dev@gmail.com>
2024-02-10 14:22:23 +0530

    implemented add message box

--- components/Playground.tsx
+++ components/Playground.tsx
@@ -41,6 +41,19 @@ const PlaygroundArea: React.FC<PlaygroundAreaProps> = ({ children }) => {
   const [nodes, setNodes, onNodesChange] = useNodesState<NodeData>(initialNodes)
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
 
+  const handleClick = () => {
+    setNodes((prevNodes) => [
+      ...prevNodes, 
+      {
+        id: `${prevNodes.length + 1}`, // Access length property directly
+        type: 'textUpdater',
+        position: { x: 0, y: (prevNodes[prevNodes.length-1]).position.y + 100},
+        data: { label: `${prevNodes.length + 1}` }, 
+      },
+    ]);
+  };
+  
+
   const onConnect = useCallback(
     (params: Parameters<typeof addEdge>[0]) =>
       setEdges((eds) => addEdge(params, eds)),
@@ -48,18 +61,19 @@ const PlaygroundArea: React.FC<PlaygroundAreaProps> = ({ children }) => {
   )
 
   return (
-      <div style={{ width: '100vw', height: '100vh' }}>
-        <ReactFlow
-          nodes={nodes}
-          edges={edges}
-          nodeTypes={nodeTypes}
-          onNodesChange={onNodesChange}
-          onEdgesChange={onEdgesChange}
-          onConnect={onConnect}
-        >
-          {children}
-        </ReactFlow>
-      </div>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        nodeTypes={nodeTypes}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
+        {children}
+      </ReactFlow>
+      <button onClick={() => handleClick() }>Click Me</button>
+    </div>
   )
 }
 
