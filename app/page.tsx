541d7cbb2c402b424a498fd4692eb8dee2c0c7d6
ashishsunny <ashishsunny.dev@gmail.com>
2024-02-11 17:36:11 +0530

    Resolved background variant type error

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
 'use client'
-import { useState } from "react"
-import SettingsPanel from "@/components/Settings"
-import Playground from "@/components/Playground"
-import PlaygroundContextProvider, {usePlaygroundContext} from '@/contexts/sendMessageContextHandler'
-import ReactFlow, {
-  MiniMap,
-  Controls,
-  Background} from 'reactflow'
+import { useState } from 'react'
+import SettingsPanel from '@/components/Settings'
+import Playground from '@/components/Playground'
+enum BackgroundVariant {
+  Lines = 'lines',
+  Dots = 'dots',
+  Cross = 'cross',
+}
+
+import { MiniMap, Controls, Background } from 'reactflow'
 
 const App = () => {
   return (
-    <PlaygroundContextProvider>
     <div className="w-screen h-screen">
       <Playground>
-        <Controls/> 
-        <MiniMap style={{zIndex:"100"}}/>
-        <Background variant="dots" gap={12} size={1} />
-        <SettingsPanel/>
+        <Controls />
+        <MiniMap style={{ zIndex: '100' }} />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+        <SettingsPanel />
       </Playground>
     </div>
-    </PlaygroundContextProvider>
   )
 }
 
